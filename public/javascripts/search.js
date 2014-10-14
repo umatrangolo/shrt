283f9aa94d353b0ee8f7090be20329fb0d6fe3a3
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2014-10-14 22:54:58 +0100

    First skeleton of the Search UI

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,3 +1,18 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+(function() {
+  var search = angular.module('search', []);
+
+  search.controller('SearchController', function() {
+    this.shrts = mocks;
+  });
+
+  // TODO
+  // Mock data to test the UI
+  var mocks = [
+    {"url":"http://www.microsoft.com","shrt":"7RWD80","count":3},
+    {"url":"http://www.gilt.com","shrt":"dIqokj","count":0},
+    {"url":"http://www.yahoo.com","shrt":"9vsDBv","count":0},
+    {"url":"http://www.google.com","shrt":"QmkGT5","count":0},
+    {"url":"http://www.facebook.com","shrt":"vjgjol","count":0}
+  ];
+  // TODO
+})();
