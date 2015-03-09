9529011cad7633eddb33bb07a4398e4a18426038
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2015-03-09 09:47:15 +0000

    Revert "Enable H2 full text search"
    H2 full text search does not support wildcard search!
    
    This reverts commit 26a02a6da45b753eb666ed6b969d080fcfe5c2c7.

--- conf/evolutions/shrt/2.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
--- Enable nativa full text search
-CREATE ALIAS IF NOT EXISTS FT_INIT FOR "org.h2.fulltext.FullText.init";
-CALL FT_INIT();
-CALL FT_CREATE_INDEX('PUBLIC', 'TEST', NULL);
-
-# --- !Downs
--- nop
