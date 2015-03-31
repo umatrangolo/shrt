b4e5b67ea5123084140f139d65b69b77455a13d0
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2015-04-01 00:27:36 +0100

    Support for incremental search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,18 +1,26 @@
 (function() {
     var search = angular.module('search', []);
 
-    search.controller('SearchController', ['$http', '$log', function($http, $log) {
+    search.controller('SearchController', ['$scope', '$http', '$log', function($scope, $http, $log) {
         var self = this;
 
         self.shrts = [];
         $http.get('/shrts/popular?k=10').success(function(data) {
-            $log.info("Popular shrt are: " + data);
+            $log.info("Popular shrt are: " + JSON.stringify(data, null, 2));
             self.shrts = data;
         });
 
-        this.lookup = function() {
-            console.log("!");
+        $scope.lookup = function() {
             $log.info("query: " + $scope.query);
+            $http.get("/shrts/completions?q=" + $scope.query).success(function(data) {
+                $log.info("Completions are: " + JSON.stringify(data));
+                var q = _.reduce(data, function(z, e) { return z + "+" + e; }, "");
+                $log.info("Searching for: " + q);
+                $http.get("/shrts?q=" + q).success(function(data) {
+                    $log.info("Matches are: " + JSON.stringify(data, null, 2));
+                    self.shrts = data;
+                });
+            });
         };
     }]);
 
